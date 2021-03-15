95d460732586ced9aa39c5843973199ef6e2e34a
Pacifique Ntahurunga <np@npacifique.us>
2021-03-15 00:39:29 -0400

    feat : upload

---
+++ GuitarShop/Models/ProductListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GuitarShop.Models
+{
+    public class ProductListViewModel
+    {
+        public ProductListViewModel()
+        {
+        }
+
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+        public string SelectedCategory { get; set; }
+        public string CheckActiveCategory
+            (string category) =>
+            category == SelectedCategory ? "active" : "";
+        
+    }
+}
